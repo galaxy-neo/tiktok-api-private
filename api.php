d7e4eda2b463fa721741dd9bbac87f0631e96e70
alethino <gneo0194@gmail.com>
2025-09-02 21:06:23 +0300

    Update api.php

--- api.php
+++ api.php
@@ -1,4 +1,4 @@
 ### contact me
-**Contact for api :** [https://t.me/Neo_Galaxy](https://t.me/Neo_Galaxy)
+**Contact for api :** [https://t.me/alethino](https://t.me/alethino)
 
-## telegram: @Neo_Galaxy
+## telegram: @alethino
